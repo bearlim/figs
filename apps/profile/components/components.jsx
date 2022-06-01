d185ba29b983ad89129e53f2799b3eb7d7c1cf16
Bearlim <brutchuces@outlook.com>
2022-05-31 22:27:23 -0300

    [Update] Styled Components

---
+++ apps/profile/components/components.jsx
@@ -0,0 +1,102 @@
+import styled from "styled-components";
+import { Dimensions, StyleSheet } from "react-native";
+
+export const CircleProfileStyle = styled.View`
+  border-radius: ${Math.round(
+    Dimensions.get("window").width + Dimensions.get("window").height
+  ) / 2}px;
+  width: ${Dimensions.get("window").width * 2}px;
+  height: ${Dimensions.get("window").width * 2}px;
+  background: hsl(340, 80%, 36.1%);
+  justify-content: center;
+  align-items: center;
+  top: -470px;
+  position: absolute;
+`;
+
+export const ContainerPicture = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background: #e23e57;
+  padding: 7px;
+  border-radius: ${Math.round(
+    Dimensions.get("window").width + Dimensions.get("window").height
+  ) / 2}px;
+  max-width: ${Math.round(Dimensions.get("window").width) * 1.5}px;
+  max-height: ${Math.round(Dimensions.get("window").height) * 0.2}px;
+`;
+
+export const Pencil = styled.View`
+  background: #000;
+  padding: 2px;
+  border-radius: ${Math.round(
+    Dimensions.get("window").width + Dimensions.get("window").height
+  ) / 2}px;
+  top: 20px;
+`;
+
+export const BorderPencil = styled.View`
+  border-radius: ${Math.round(
+    Dimensions.get("window").width + Dimensions.get("window").height
+  ) / 2}px;
+  width: ${Dimensions.get("window").width * 0.15}px;
+  height: ${Dimensions.get("window").width * 0.15}px;
+  background: hsl(351, 80%, 56%);
+  justify-content: center;
+  align-items: center;
+  opacity: 60;
+`;
+
+export const style = StyleSheet.create({
+  Picture: {
+    height: 150,
+    width: 150,
+    alignSelf: "center",
+    borderRadius:
+      Math.round(
+        Dimensions.get("window").width + Dimensions.get("window").height
+      ) / 2,
+  },
+
+  CircleShadow: {
+    shadowColor: "#171717",
+    shadowOffset: { width: 50, height: 50 },
+    shadowOpacity: 100,
+    shadowRadius: 50,
+  },
+
+  ProfileNameStyle: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 23,
+    top: -20,
+  },
+
+  ContainerDashBoard: {
+    flex: 1,
+    top: 180,
+    // replica do dysplay inline
+    flexWrap: "wrap",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+
+  BorderIconDashboard: {
+    backgroundColor: "hsl(340, 80%, 36.1%)",
+    borderRadius:
+      Math.round(
+        Dimensions.get("window").width + Dimensions.get("window").height
+      ) / 2,
+    height: 63,
+    width: 63,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  IconsDashboard: {
+    alignSelf: "center",
+  },
+});
--- apps/profile/styleProfile.jsx
+++ apps/profile/styleProfile.jsx
@@ -9,21 +9,18 @@ import {
   Alert,
 } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import SimpleIcon from "react-native-vector-icons/SimpleLineIcons";
-import Octicons from "react-native-vector-icons/Octicons";
 import { Dashboard } from "./components/dashboard";
+import {
+  CircleProfileStyle,
+  ContainerPicture,
+  Pencil,
+  BorderPencil,
+  style,
+} from "./components/components";
 import styled from "styled-components";
 
 export const CircleProfile = () => {
-  return (
-    <View
-      style={[
-        style.CircleProfile,
-        style.CircleShadow,
-        { position: "absolute" },
-      ]}
-    ></View>
-  );
+  return <CircleProfileStyle></CircleProfileStyle>;
 };
 
 export const CircleProfilePicture = ({ uriPicture }) => {
@@ -50,11 +47,11 @@ export const ProfileName = ({ nmUser }) => {
 export const EditProfile = ({ idProfile }) => {
   return (
     <TouchableOpacity onPress={OnPressEditProfile}>
-      <View style={style.Pencil}>
-        <View style={style.BorderPencil}>
+      <Pencil style={style.Pencil}>
+        <BorderPencil style={style.BorderPencil}>
           <Icon name="pencil" color="#311D3F" size={45} />
-        </View>
-      </View>
+        </BorderPencil>
+      </Pencil>
     </TouchableOpacity>
   );
 };
@@ -66,105 +63,3 @@ const OnPressEditProfile = ({ idProfile }) => {
 export const DashboardProfile = ({ figsCount, ConqCount }) => {
   return <Dashboard figsCount={figsCount} ConqCount={ConqCount} />;
 };
-
-const ContainerPicture = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background: #e23e57;
-  padding: 7px;
-  border-radius: ${Math.round(
-    Dimensions.get("window").width + Dimensions.get("window").height
-  ) / 2}px;
-  max-width: ${Math.round(Dimensions.get("window").width) * 1.5}px;
-  max-height: ${Math.round(Dimensions.get("window").height) * 0.2}px;
-`;
-
-const style = StyleSheet.create({
-  CircleProfile: {
-    borderRadius:
-      Math.round(
-        Dimensions.get("window").width + Dimensions.get("window").height
-      ) / 2,
-    width: Dimensions.get("window").width * 2,
-    height: Dimensions.get("window").width * 2,
-    backgroundColor: "hsl(340, 80%, 36.1%)",
-    justifyContent: "center",
-    alignItems: "center",
-    top: -470,
-  },
-
-  Picture: {
-    height: 150,
-    width: 150,
-    alignSelf: "center",
-    borderRadius:
-      Math.round(
-        Dimensions.get("window").width + Dimensions.get("window").height
-      ) / 2,
-  },
-
-  CircleShadow: {
-    shadowColor: "#171717",
-    shadowOffset: { width: 50, height: 50 },
-    shadowOpacity: 100,
-    shadowRadius: 50,
-  },
-
-  ProfileNameStyle: {
-    color: "white",
-    fontWeight: "500",
-    fontSize: 23,
-    top: -20,
-  },
-
-  Pencil: {
-    backgroundColor: "#000",
-    padding: 2,
-    borderRadius:
-      Math.round(
-        Dimensions.get("window").width + Dimensions.get("window").height
-      ) / 2,
-    top: 20,
-  },
-
-  BorderPencil: {
-    borderRadius:
-      Math.round(
-        Dimensions.get("window").width + Dimensions.get("window").height
-      ) / 2,
-    width: Dimensions.get("window").width * 0.15,
-    height: Dimensions.get("window").width * 0.15,
-    backgroundColor: "hsl(351, 80%, 56%)",
-    justifyContent: "center",
-    alignItems: "center",
-    opacity: 60,
-  },
-
-  ContainerDashBoard: {
-    flex: 1,
-    top: 180,
-    // replica do dysplay inline
-    flexWrap: "wrap",
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-
-  BorderIconDashboard: {
-    backgroundColor: "hsl(340, 80%, 36.1%)",
-    borderRadius:
-      Math.round(
-        Dimensions.get("window").width + Dimensions.get("window").height
-      ) / 2,
-    height: 63,
-    width: 63,
-    padding: 10,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  IconsDashboard: {
-    alignSelf: "center",
-  },
-});
