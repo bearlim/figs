23cf37185bca9c645a7af0c690edb3fbaa5496f6
Tatiana Albuquerque <taticavalcantihcp@gmail.com>
2022-06-11 00:23:35 -0300

    adicionando a tela da figurinha

--- src/routes/app.route.jsx
+++ src/routes/app.route.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProfileScreen from "../../apps/profile/profile";
 import AlbumScreen from "../../apps/albumscreen/album";
+import FigurinhaScreen from "../../apps/figurinha/figurinha";
 import { createStackNavigator } from "@react-navigation/stack";
 import ScannerQr from "../../apps/qrCode/Scanner";
 import NavBar from "../../apps/navbar/navbar";
@@ -10,16 +11,15 @@ const AppStack = createStackNavigator();
 
 const AppRoutes = () => {
   return (
-    // <AppStack.Navigator
-    //   screenOptions={{ headerShown: false }}
-    //   initialRouteName="AlbumScreen"
-    // >
-    //   <AppStack.Screen name="ProfileScreen" component={ProfileScreen} />
-    //   <AppStack.Screen name="AlbumScreen" component={AlbumScreen} />
-    //   <AppStack.Screen name="QrCode" component={ScannerQr} />
-    //   <NavBar />
-    // </AppStack.Navigator>
-    <NavBar />
+    <AppStack.Navigator 
+    screenOptions={{ headerShown: false }}
+    initialRouteName="AlbumScreen"
+    >
+      <AppStack.Screen name="ProfileScreen" component={ProfileScreen} />
+      <AppStack.Screen name="AlbumScreen" component={AlbumScreen} />
+      <AppStack.Screen name="QrCode" component={ScannerQr} />
+      <AppStack.Screen name="FigurinhaScreen" component={FigurinhaScreen} />
+    </AppStack.Navigator>
   );
 };
 
