8e8e4fb47db28cc1192527006bee8765fb154b77
felixpaulo <ph_felix08@hotmail.com>
2022-06-17 19:34:03 -0300

    Cadastro de clientes e figurinhas

---
+++ apps/cadastro/styles.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        // backgroundColor: 'red',
+        width: '100%',
+        height: '100%',
+    },
+    boxInput:{
+        width: '90%',
+        marginHorizontal: 20,
+        top: 200
+    },
+    textLabel:{
+        margin: 10,
+    },
+    inputText:{
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 6,
+        padding: 10
+    },
+    buttonSave:{
+        backgroundColor: '#88304E',
+        padding: 15,
+        marginTop: 50,
+        borderRadius: 6
+
+    },
+    textButton: {
+        textAlign: 'center',
+        color: 'white',
+    }
+})
--- apps/figurinha/figurinha.jsx
+++ apps/figurinha/figurinha.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { View, Text, Button, Image } from "react-native";
+import { Textopadrao, Titulofig, BoxInfo, InfosDetalhes, TextSobre, BoxSobre } from './style';
+import Ionicons from "react-native-vector-icons/Ionicons"
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 function FigurinhaScreen({ navigation }) {
   return (
@@ -8,14 +12,36 @@ function FigurinhaScreen({ navigation }) {
           style={{ width: 400, height: 450, marginTop: 50 }}
           source={require("../assets/neymar.jpeg")}
         />
-              <Text style={{ marginTop: 1, fontSize: 30 }}>Neymar Jr, 30</Text>
+              <BoxInfo>
+                <Titulofig>
+                  <Text style={{ marginTop: 1, fontSize: 30, marginRight: 8, color:"rgba(0,0,0,0.76)" }}>Neymar Jr, 30</Text>
+                  <FontAwesome name={'diamond'} size={18} color={'#FFD233'}></FontAwesome>
+                </Titulofig>
+                <InfosDetalhes>
+                  <Ionicons name={'football'} color={"rgba(0,0,0,0.6)"} size={16}></Ionicons>
+                  <Text style={{marginLeft: 4}}>Jogador de Futebol</Text>
+                </InfosDetalhes>
+                <InfosDetalhes>
+                  <Ionicons name={'md-star-sharp'} color={"rgba(0,0,0,0.6)"}  size={16}></Ionicons>
+                  <Text style={{marginLeft: 4}}>Raridade: lendário</Text>
+                </InfosDetalhes>
+                <InfosDetalhes>
+                  <MaterialCommunityIcons name={'book-outline'} color={"rgba(0,0,0,0.6)"}  size={16}></MaterialCommunityIcons>
+                  <Text style={{marginLeft: 4}}>Álbum: futebol</Text>
+                </InfosDetalhes>
 
-              <Text>Jogador de Futebol</Text>
-              <Text>Raridade: lendário</Text>
-              <Text>Álbum: futebol</Text>
+                <BoxSobre>
+                  <Text style={{ marginBottom: 4, fontSize: 25 }}>Sobre</Text>
+                  <TextSobre>
+                    <Text style={{textAlign: "justify"}}>orem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu luctus sapien. Integer at suscipit erat. Morbi a rhoncus ante. Sed volutpat euismod elit, non lobortis augue mattis non. Etiam vitae odio efficitur, feugiat arcu sed, hendrerit nisi. Quisque eget hendrerit tortor. Phasellus vestibulum est quis tortor viverra posuere. Donec interdum lectus vitae mi blandit elementum. Curabitur bibendum diam at vulputate tincidunt.</Text>
+                  </TextSobre>
+                </BoxSobre>
 
-              <Text style={{ marginTop: 1, fontSize: 25 }}>Sobre</Text>
-              <Text>orem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu luctus sapien. Integer at suscipit erat. Morbi a rhoncus ante. Sed volutpat euismod elit, non lobortis augue mattis non. Etiam vitae odio efficitur, feugiat arcu sed, hendrerit nisi. Quisque eget hendrerit tortor. Phasellus vestibulum est quis tortor viverra posuere. Donec interdum lectus vitae mi blandit elementum. Curabitur bibendum diam at vulputate tincidunt.</Text>
+
+              </BoxInfo>
+              
+              
+
     </View>
   );
 }
