efe934521a43a93f7034b0fea3663bf58e782943
felixpaulo <ph_felix08@hotmail.com>
2022-06-24 08:48:47 -0300

    figurinhas

--- apps/figurinha/style.tsx
+++ apps/figurinha/style.tsx
@@ -1,35 +1,35 @@
-import styled from "styled-components";
-// import { StyleSheet, View } from "react-native";
+// import styled from "styled-components";
+// // import { StyleSheet, View } from "react-native";
 
-export const Textopadrao = styled.View`
-    background: red;
-`;
+// export const Textopadrao = styled.View`
+//     background: red;
+// `;
 
-export const Titulofig = styled.View`
-    margin-bottom: 34px;
-    flex-direction: row;
-    align-items: center;
-    margin-right: 10px;
-`;
+// export const Titulofig = styled.View`
+//     margin-bottom: 34px;
+//     flex-direction: row;
+//     align-items: center;
+//     margin-right: 10px;
+// `;
 
-export const BoxInfo = styled.View`
-    margin-top: 26px;
-    margin-left: 23px;
-    margin-right: 16px;
-    // margin-bottom: 64px;
+// export const BoxInfo = styled.View`
+//     margin-top: 26px;
+//     margin-left: 23px;
+//     margin-right: 16px;
+//     // margin-bottom: 64px;
 
-`;
+// `;
 
-export const InfosDetalhes= styled.View`
-    flex-direction: row;
-    align-items: center;
-    margin-bottom: 11px;
-`;
+// export const InfosDetalhes= styled.View`
+//     flex-direction: row;
+//     align-items: center;
+//     margin-bottom: 11px;
+// `;
 
-export const TextSobre = styled.View`
-    text-align: justify;
-`
+// export const TextSobre = styled.View`
+//     text-align: justify;
+// `
 
-export  const BoxSobre = styled.View`
-    margin-top: 36px;
-`
+// export  const BoxSobre = styled.View`
+//     margin-top: 36px;
+// `
--- src/routes/app.route.jsx
+++ src/routes/app.route.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ProfileScreen from "../../apps/profile/profile";
-<<<<<<< HEAD
 import FigurinhaScreen from "../../apps/figurinha/figurinha";
-=======
 import AlbumScreen from "../../apps/albumscreen/album";
->>>>>>> 3296cde4ce905774670aabf15eb4529c9b2356e5
 import { createStackNavigator } from "@react-navigation/stack";
 import ScannerQr from "../../apps/qrCode/Scanner";
 import NavBar from "../../apps/navbar/navbar";
@@ -14,12 +11,10 @@ const AppStack = createStackNavigator();
 
 const AppRoutes = () => {
   return (
-<<<<<<< HEAD
     <AppStack.Navigator screenOptions={{ headerShown: false }}>
       <AppStack.Screen name="ProfileScreen" component={ProfileScreen} />
       <AppStack.Screen name="FigurinhaScreen" component={FigurinhaScreen} />
     </AppStack.Navigator>
-=======
     // <AppStack.Navigator
     //   screenOptions={{ headerShown: false }}
     //   initialRouteName="AlbumScreen"
@@ -29,8 +24,7 @@ const AppRoutes = () => {
     //   <AppStack.Screen name="QrCode" component={ScannerQr} />
     //   <NavBar />
     // </AppStack.Navigator>
-    <NavBar />
->>>>>>> 3296cde4ce905774670aabf15eb4529c9b2356e5
+    //<NavBar />
   );
 };
 
