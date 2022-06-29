81fd6f18577d46f623726669f242b441a5dfcea8
Bearlim <brutchuces@outlook.com>
2022-06-28 21:34:23 -0300

    [Update] Forgot Pass

--- apps/recuperarSenha/recuperarSenha.jsx
+++ apps/recuperarSenha/recuperarSenha.jsx
@@ -1,44 +1,24 @@
-import React, { useState } from "react"
-import { View, Text, TextInput, Pressable, Image } from "react-native"
-import { styles } from "./recuperarSenhaStyle"
+import React, { useState } from "react";
+import { Image } from "react-native";
+import { StyledInput, Br, DialogBox } from "../../style/global/styleGlobal";
+import { FlatButton } from "../../components/button/button";
+import { Container } from "./recuperarSenhaStyle";
 
-export function Teste(){
+export function ForgotPassScreen() {
+  const [email, setEmail] = useState("");
 
-    const [nickname, setNickname] = useState('')
-    const [nome, setNome] = useState('')
-    const [email, setEmail] = useState('')
+  function salvarNickName() {}
 
-    function salvarNickName(){
-        console.log('Apertei')
-    }
-
-    return(
-        <>
-            <View style={styles.container}>
-
-                <View style={styles.boxInput}>
-  
-                    <Image
-                    
-                    source={require('./vetor.png')}
-
-                    />
-
-                    <Text style={styles.textLabel}>Digite seu E-mail para recuperação da senha</Text>
-                    <TextInput 
-                        style={styles.inputText}
-                        onChangeText={setEmail}
-                        value={email}
-                    ></TextInput>
-
-                    <Pressable 
-                        style={styles.buttonSave}
-                        onPress={salvarNickName}
-                    >
-                        <Text style={styles.textButton}>Enviar</Text>
-                    </Pressable>
-                </View>
-            </View>
-        </>
-    )
+  return (
+    <Container>
+      <Image source={require("./vetor.png")} />
+      <Br />
+      <StyledInput
+        label="Digite seu E-mail para recuperação da senha"
+        placeholder="Digite o seu e-mail"
+        func={setEmail}
+      />
+      <FlatButton text="Recuperar" func={salvarNickName} />
+    </Container>
+  );
 }
--- apps/recuperarSenha/recuperarSenhaStyle.jsx
+++ apps/recuperarSenha/recuperarSenhaStyle.jsx
@@ -1,41 +1,8 @@
-import { StyleSheet } from "react-native";
+import styled from "styled-components";
 
-export const styles = StyleSheet.create({
-    container: {
-        // backgroundColor: 'red',
-        width: '100%',
-        height: '100%',
-    },
-    boxInput:{
-        width: '90%',
-        marginHorizontal: 20,
-        top: 200
-    },
-    textLabel:{
-        margin: 10,
-        fontWeight: 'bold',
-        fontSize:16,
-    },
-    inputText:{
-        
-        borderColor: 'gray',
-        borderWidth: 1,
-        borderRadius: 6,
-        padding: 10
-    },
-    buttonSave:{
-        backgroundColor: '#B92D10',
-        padding: 15,
-        marginTop: 50,
-        borderRadius: 17,
-        width:355,
-        height: 65
-    },
-    textButton: {
-        fontSize: 16,
-        paddingTop: 5 ,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        color: 'white',
-    }
-})
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  top: 25px;
+  margin: 25px;
+`;
