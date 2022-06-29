838ea66726fd59be48fca7f4f00d936fbb7aa2b7
Bearlim <brutchuces@outlook.com>
2022-06-28 21:36:09 -0300

    [Update] Config Screen

--- apps/config/config.jsx
+++ apps/config/config.jsx
@@ -1,5 +1,14 @@
 import React, { useContext } from "react";
-import { View, SafeAreaView, FlatList, Text, StyleSheet, StatusBar, Button, Image } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+  Button,
+  Image,
+} from "react-native";
 import SimpleIcon from "react-native-vector-icons/SimpleLineIcons";
 import {
   CircleProfile,
@@ -7,81 +16,73 @@ import {
   ProfileName,
 } from "../profile/components/styleProfile";
 import AuthenticationContext from "../../src/contexts/authentication";
-import { Container } from "../../style/global/styleGlobal";
+import { Container, DialogBox } from "../../style/global/styleGlobal";
 import { ScrollView } from "react-native-gesture-handler";
 
 const dataPreferencias = [
   {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'Opção 1',
-    iconName: 'folder',
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "Opção 1",
+    iconName: "folder",
   },
   {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Opção 2',
-    iconName: 'eye',
-
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Opção 2",
+    iconName: "eye",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Opção 3',
-    iconName: 'question',
-
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Opção 3",
+    iconName: "question",
   },
 ];
 
 const dataConteudos = [
   {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad42423bb28ba',
-    title: 'Opção 11',
-    iconName: 'picture',
+    id: "bd7acbea-c1b1-46c2-aed5-3ad42423bb28ba",
+    title: "Opção 11",
+    iconName: "picture",
   },
   {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad42423badasdasfa',
-    title: 'Opção 11',
-    iconName: 'user',
-  },{
-    id: 'bd7acbea-c1b1-46c2-aed5-3adsadasda23bb28ba',
-    title: 'Opção 11',
-    iconName: 'lock',
+    id: "bd7acbea-c1b1-46c2-aed5-3ad42423badasdasfa",
+    title: "Opção 11",
+    iconName: "user",
+  },
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3adsadasda23bb28ba",
+    title: "Opção 11",
+    iconName: "lock",
   },
 ];
 
 const Item = ({ title, iconName }) => (
-
   <View style={styles.item}>
-
     <SimpleIcon name={iconName} style={styles.iconOptions} size={22} />
 
     <Text style={styles.title}>{title}</Text>
     <SimpleIcon name="arrow-right" style={styles.iconNext} size={15} />
-
   </View>
 );
 
 const BoxConfig = ({ name, data }) => {
-
   const renderItem = ({ item }) => (
     <Item title={item.title} iconName={item.iconName} />
   );
 
   return (
-      <View style={styles.box}>
+    <View style={styles.box}>
+      <Text style={styles.section}> {name} </Text>
 
-        <Text style={styles.section}> {name} </Text>
-
-        <FlatList
-          data={data}
-          renderItem={renderItem}
-          keyExtractor={item => item.id}
-        />
-
-      </View>
-  )
-}
-function ConfigScreenT({ }) {
-
-  const { user } = useContext(AuthenticationContext);
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+};
+function ConfigScreenT({}) {
+  const { user, SignOutRequest } = useContext(AuthenticationContext);
 
   return (
     <ScrollView>
@@ -93,26 +94,37 @@ function ConfigScreenT({ }) {
           <CircleProfilePicture uriPicture={"user.image_url"} />
         </View>
 
-        <View style={{alignItems: 'center'}}>
-          {/* <ProfileName nmUser={"user.name"} /> */}
+        <View style={{ alignItems: "center" }}>
           <Text style={styles.userNametitle}>@Barleam</Text>
           <Text style={styles.userEmailtitle}>Yuri@gmail.com</Text>
         </View>
 
         <View style={styles.editProfileButton}>
           <Text style={styles.titleEditProfile}>Editar Perfil</Text>
-          <SimpleIcon name="arrow-right" style={styles.iconNextEdit} size={15} />
+          <SimpleIcon
+            name="arrow-right"
+            style={styles.iconNextEdit}
+            size={15}
+          />
         </View>
-
       </Container>
 
       <BoxConfig name={"CONTEÚDOS"} data={dataConteudos}></BoxConfig>
       <BoxConfig name={"PREFERÊNCIAS"} data={dataPreferencias}></BoxConfig>
+
+      <DialogBox
+        func={SignOutRequest}
+        title="Sair"
+        text={
+          "Tem certeza que deseja sair? Será necessário fazer o login novamente"
+        }
+        event="Sair"
+      />
     </ScrollView>
   );
 }
 
-const greyColor = '#708090';
+const greyColor = "#708090";
 
 const styles = StyleSheet.create({
   container: {
@@ -121,21 +133,20 @@ const styles = StyleSheet.create({
   },
   item: {
     padding: 10,
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingLeft: 37,
     marginTop: 7,
-    
   },
 
-  box: { 
-    paddingRight:10, 
+  box: {
+    paddingRight: 10,
     paddingLeft: 10,
   },
 
   title: {
     fontSize: 20,
     paddingLeft: 10,
-    width: 100
+    width: 100,
   },
 
   section: {
@@ -145,54 +156,51 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 16,
     paddingLeft: 30,
     paddingVertical: 9,
-    alignItems: 'center'
+    alignItems: "center",
   },
 
-  iconNext:{
-    backgroundColor: 'rgba(185, 45, 16, 0.63)',
+  iconNext: {
+    backgroundColor: "rgba(185, 45, 16, 0.63)",
     borderRadius: 100,
     padding: 5,
   },
 
-  iconOptions:{
-    color: 'rgba(5, 0, 56, 0.5)',
+  iconOptions: {
+    color: "rgba(5, 0, 56, 0.5)",
   },
 
-  editProfileButton:{
+  editProfileButton: {
     backgroundColor: "rgba(185, 45, 16, 1)",
-    flexDirection: 'row',
+    flexDirection: "row",
     padding: 8,
     borderRadius: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginBottom: 15,
-    marginTop: 15
-
+    marginTop: 15,
   },
 
-  titleEditProfile:{
-    color: 'white',
+  titleEditProfile: {
+    color: "white",
     fontSize: 20,
     paddingHorizontal: 10,
   },
 
-  iconNextEdit:{
+  iconNextEdit: {
     backgroundColor: "rgba(255, 255, 255, 0.21)",
     borderRadius: 20,
     padding: 5,
-    shadowOffset: {width: 4, height: 10},
+    shadowOffset: { width: 4, height: 10 },
     shadowColor: "rgba(255, 255, 255, 0.21)",
-
   },
 
-  userNametitle:{
-    fontSize: 40
+  userNametitle: {
+    fontSize: 40,
   },
-  userEmailtitle:{
+  userEmailtitle: {
     fontSize: 16,
-    marginTop: 10
-  }
-
+    marginTop: 10,
+  },
 });
 
 export default ConfigScreenT;
--- src/routes/auth.route.jsx
+++ src/routes/auth.route.jsx
@@ -3,6 +3,7 @@ import LoginScreen from "../../apps/login/login";
 import { StatusBar } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { RegisterScreen } from "../../apps/registration/register";
+import { ForgotPassScreen } from "../../apps/recuperarSenha/recuperarSenha";
 
 const AuthStack = createStackNavigator();
 
@@ -11,6 +12,7 @@ const AuthRoutes = () => {
     <AuthStack.Navigator screenOptions={{ headerShown: false }}>
       <AuthStack.Screen name="LoginScreen" component={LoginScreen} />
       <AuthStack.Screen name="RegistrationScreen" component={RegisterScreen} />
+      <AuthStack.Screen name="ForgotPassScreen" component={ForgotPassScreen} />
     </AuthStack.Navigator>
   );
 };
